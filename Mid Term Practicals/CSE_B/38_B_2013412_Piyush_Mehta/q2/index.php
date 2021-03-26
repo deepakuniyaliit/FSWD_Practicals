79cfa3a04a2155e5fa1e4157eb74db4c7c2c8d2c
Piyush <piyuinshort@gmail.com>
2021-03-26 11:03:32 +0530

    modify the HTML form to use post method && create database connection using php

--- Mid Term Practicals/CSE_B/38_B_2013412_Piyush_Mehta/q2/index.php
+++ Mid Term Practicals/CSE_B/38_B_2013412_Piyush_Mehta/q2/index.php
@@ -1,50 +1,56 @@
-<html>
-  <head>
-    <title>FullStackPratical2</title>
-  </head>
-  <body>
-    <form action="index.php" method="GET">
-      <h3 class="a">Please Enter Your Details</h3>
-      <h4 class="a">Fill the application form below and submit</h4>
-      Username
-      <input
-        type="text"
-        name="Username"
-        placeholder="Username"
-        required
-      /><br />
-      Email address
-      <input
-        type="email"
-        name="email"
-        placeholder="mail@example.com"
-        required
-      /><br />
-      Phone Number
-      <input
-        type="number"
-        maxlength="10"
-        name="pno"
-        placeholder="Phone Number"
-        required
-      /><br />
-      Gender <input type="radio" name="gender" value="Male" required />Male
-      <input type="radio" name="gender" value="Female" required />Female
-      <input type="radio" name="gender" value="Other" required />Other <br />
-      City
-      <select id="city" name="city">
-        <option selected>Mumbai</option>
-        <option>Delhi</option>
-        <option>Jaipur</option>
-        <option>Mussoorie</option>
-        <option>Nanital</option>
-        <option>Chennai</option>
-        <option>Pune</option>
-        <option>Bangalore</option>
-        <option>Hyderabad</option>
-        <option>Lucknow</option></select
-      ><br />
-      <input type="submit" value="submit" />
+<?php
+include('config.php');
+?>
+
+<?php
+if(isset($_POST['submit']))
+{
+	
+	$username=$_POST['username'];
+	$email=$_POST['email'];
+    $contact=$_POST['contact'];
+	$gender=$_POST['gender'];
+	$city=$_POST['city'];
+	
+	$sql="INSERT INTO `users`( `username`, `email`, `contact`,`gender`, `city`) VALUES ('$username','$contact',$email','$gender','$city')";
+	mysqli_query($conn,$sql);
+}
+else
+{
+	echo "Please provide input";
+}
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form</title>
+</head>
+<body>
+    <form method="post"  action="index.php">
+        Username<input type="text" name="username" required><br>
+        Email<input type="email"  name="email"  required><br>
+        Contact<input type="text"   name="contact" required><br>
+        Gender:<br>
+        Male<input type="radio" id="male" name="gender" value="male"><br>
+        
+        Female<input type="radio" id="female" name="gender" value="female"><br>
+        Other<input type="radio" id="other" name="gender" value="other"><br>
+        Select City<select name="city" >
+            <option value="Mumbai">Mumbai</option>
+            <option value="Delhi">Delhi</option>
+            <option value="Jaipur">Jaipur</option>
+            <option value="Mussorie">Mussorie</option>
+            <option value="Nainital">Nainital</option>
+            <option value="Chennai">Chennai</option>
+            <option value="Pune">Pune</option>
+            <option value="Banglore">Banglore</option>
+            <option value="Hydrabad">Hydrabad</option>
+            <option value="Lucknow">Lucknow</option>
+          </select><br>
+          <input type="submit"  value="Submit">
+
     </form>
-  </body>
+</body>
 </html>
