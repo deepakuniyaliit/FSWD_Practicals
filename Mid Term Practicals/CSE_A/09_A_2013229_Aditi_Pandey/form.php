ff828f7828c2617301146217c97ca996fd3d36f6
aditipandey-prog <aditipandey394@gmail.com>
2021-03-26 11:19:32 +0530

    modify the form using post ..the first form.htm was for get method

---
+++ Mid Term Practicals/CSE_A/09_A_2013229_Aditi_Pandey/form.php
@@ -0,0 +1,69 @@
+<?php
+include('configuration.php')
+?>
+<?php
+ // Selecting Database from Server
+if(isset($_POST['submit'])){ // Fetching variables of the form which travels in URL
+$name = $_POST['name'];
+$email = $_POST['email'];
+$city = $_POST['city'];
+$gender=$_POST['gender'];
+
+$query = "INSERT INTO `users` (`name`,`email`,`gender`,`city`) values ('$name', '$email', '$gender', '$city')";
+mysqli_query($conn, $query);
+
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="form.php" method="POST">
+        <p>
+            <label for="name">Name*:</label>
+            <input type="text" name="name" id="iname" required="">
+        </p>
+        <p>
+            <label for="Phoneno">Phone no*:</label>
+            <input type="text" id="phone_no" maxlength="10" required=""> 
+        </p>
+        <p>
+            <label for="email">Email*:</label>
+            <input type="email" name="email" id="iemail" required="" >
+        </p>
+        <p>
+            <label for="icity">City*:<sup>*</sup></label>
+            <select id="icity" name="city">
+            <option value="Mussorie">Mussorie</option>
+            <option value="Mumbai">Mumbai</option>
+            <option value="Delhi">Delhi</option>
+            <option value="Nanital">Nanital</option>
+            <option value="Pune">Pune</option>
+            <option value="Jaipur">Jaipur</option>
+            <option value="Lucknow">Lucknow</option>
+            <option value="Chennai">Chennai</option>
+            <option value="hyderabad">hyderabad</option>
+            <option value="Banglore">Banglorer</option>
+
+          </select>
+        </p>
+        <p>
+        Gender*:
+        <input type="radio" id="male" name="gender" value="male">
+        <label for="male">Male</label><br>
+        <input type="radio" id="female" name="gender" value="female">
+        <label for="female">Female</label><br>
+        <input type="radio" id="other" name="gender" value="other">
+         <label for="other">Other</label>
+        
+       </p>
+       
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+</html>
