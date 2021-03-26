a5ef898b9d1be16a134ec2a2c8800881ea7f38f0
Raghavendra <raghavendrashekhawat2@gmail.com>
2021-03-26 11:03:48 +0530

    Create configuration.php
    Created database connection

---
+++ Mid Term Practicals/CSE_A/2013435_Raghavendra_Singh_Shekhawat/configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+    $conn = mysqli_connect('localhost', 'raghavendra', 'aabbccdd', 'Webpractical');
+
+    if(!$conn){
+        echo "Connection error" . mysqli_connect_error();
+    }
+    else{
+        echo "Connection Successfull!!";
+    }
+
+?>
