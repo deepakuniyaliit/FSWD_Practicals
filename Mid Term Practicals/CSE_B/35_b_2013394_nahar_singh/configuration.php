2afd00e05ac9bfa147378cf02e66ee9fb9dc2e54
naharsingh19 <78068436+naharsingh19@users.noreply.github.com>
2021-03-26 10:50:26 +0530

    create a database connection using php

---
+++ Mid Term Practicals/CSE_B/35_b_2013394_nahar_singh/configuration.php
@@ -0,0 +1,14 @@
+<?php
+$hostname="localhost";
+$username="root";
+$password="";
+$database="Web";
+
+$conn=mysqli_connect($hostname, $username ,$password ,$database);
+if($conn){
+	echo "Connection Successful...";
+}
+else{
+	echo "Connection failed..." +mysqli_connect_error();
+}
+?>
