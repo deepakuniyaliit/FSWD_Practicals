3ca54f5d9bcbc86d6d2317e56433ee1b27ae6784
anubhavs <anubhavsolanki0@gmail.com>
2021-03-26 10:58:29 +0530

    validating data using PHP

--- Mid Term Practicals/CSE_A/62_A_2013642_anubhav_solanki/second/index.php
+++ Mid Term Practicals/CSE_A/62_A_2013642_anubhav_solanki/second/index.php
@@ -7,4 +7,13 @@ if(isset($_POST["submit"])){
     $gender = $_POST["gender"];
     $city = $_POST["city"];   
 }
+
+validation($username,$email,$contact,$gender,$city);
+
+    function validation($username,$email,$contact,$gender,$city){
+        $flag = true;
+        if(empty($username) || empty($email) || empty($contact) || empty($gender) || empty($city) || strlen($contact)!=10){
+            $flag = false;
+        }
+    }
 ?>
