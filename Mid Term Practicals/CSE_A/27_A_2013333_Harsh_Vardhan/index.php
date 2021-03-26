0619675a37a08afa02e751a6df69bf6e8ddcac9e
HarshVardhanGeu <79054610+HarshVardhanGeu@users.noreply.github.com>
2021-03-26 11:08:46 +0530

    retrieving form data while avoiding form submission on refreshing the page

---
+++ Mid Term Practicals/CSE_A/27_A_2013333_Harsh_Vardhan/index.php
@@ -0,0 +1,19 @@
+<?php
+include('configuration.php');
+ ?>
+<?php
+if(isset($_POST['submit']))
+{
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $contact=$_POST['contact'];
+  $gender=$_POST['gender'];
+  $city=$_POST['city'];
+
+  $sql="INSERT INTO `users`(`username`,`email`,`contact`,`gender`,`city`) VALUES ('$username','$email','$contact','$gender','$city')";
+  mysqli_query($conn,$sql);
+}
+else {
+  echo "Please click submit";
+}
+ ?>
