1483e2d561de72dba75cc364309661100551e9dd
Siddhartha03112000 <65247431+Siddhartha03112000@users.noreply.github.com>
2021-03-26 10:48:11 +0530

    retrieving form data while avoiding the submission on refreshing the page

--- Mid Term Practicals/CSE_A/57_A_20134525_Siddhartha_Khandelwal/html_form.php
+++ Mid Term Practicals/CSE_A/57_A_20134525_Siddhartha_Khandelwal/html_form.php
@@ -1,3 +1,18 @@
+<?php
+   require_once('configuration.php');
+   if(isset($_POST['submit'])){
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $contact = $_POST['contact'];
+        $gender = $_POST['gender'];
+        $city = $_POST['city'];
+        $sql = "INSERT INTO `users` (`username`, `email`,`contact`, `gender`, `city`) VALUES ('$username', '$email','$contact ', '$gender','$city')";
+
+        if (!(mysqli_query($conn, $sql))) {
+            echo "Invalid Query";
+        }
+   }  
+?>
 <html>
     <head>
         <title>HTML FORM</title>
@@ -9,14 +24,14 @@
             email
             <input type="email" name="email" placeholder="email" required><br>
             contact
-            <input type="tel" name="contact no." placeholder="contact no." pattern="[0-9]{10} required ><br>
+            <input type="tel" name="contact" placeholder="contact" pattern="[0-9]{10}" required ><br>
             gender
             Male<input type="radio" name="gender" value="male">
             Female<input type="radio" name="gender" value="female">
             others<input type="radio" name="gender" value="male"><br>
             City
-            <select name="City" required>
-            <option value="">City</option>
+            <select name="city" required>
+            <option value="">city</option>
             <option value="Mumbai">Mumbai</option>
             <option value="Delhi">Delhi</option>
             <option value="Jaipur">Jaipur</option>
@@ -28,7 +43,7 @@
             <option value="Hyderabad">Hyderabad</option>
             <option value="Lucknow">Lucknow</option>
             Submit
-            <input type="submit" value="Submit">
+            <input type="submit" name="submit" value="submit">
         </form>
     </body>
 </html>
