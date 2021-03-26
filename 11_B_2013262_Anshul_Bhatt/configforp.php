b1e63a4f8ea00babbb03b8bb8519067e8815aabe
Anshul <anshulbhattddn@gmail.com>
2021-03-26 10:25:56 +0530

    by mistake

--- 11_B_2013262_Anshul_Bhatt/configforp.php
+++ 11_B_2013262_Anshul_Bhatt/configforp.php
@@ -3,7 +3,7 @@
 $hostname="127.0.0.1";
 $username="root";
 $password="";
-$databasename="Web";
+$databasename="WebPractical";
 $conn=mysqli_connect($hostname,$username,$password,$databasename);
 if($conn)
 {
