721d4d86be5e6cf27519e4ea8f842817b6ef88ca
AyushKool <46340004+AyushKool@users.noreply.github.com>
2021-03-26 00:38:02 -0500

    insert data into database using PHP script

--- Mid Term Practicals/CSE_A/24_A_2013290_Ayush_Koul/index.php
+++ Mid Term Practicals/CSE_A/24_A_2013290_Ayush_Koul/index.php
@@ -10,4 +10,21 @@
         else 
             return true;
     }
+    if(isset($_POST['submit']))
+    {
+        $un = $_POST["username"];
+        $e = $_POST["email"];
+        $g = $_POST["gender"];
+        $c = $_POST["phone"]
+        $ct = $_POST["city"];
+        if(validate($un, $g, $e, $c, $ct))
+        {
+            $query = "INSERT INTO `users` (`id`, `username`, `gender`, `email`, `contact`, `city`) VALUES (NULL, '$username', '$gender', '$email', '$contact', '$city')";
+            if(mysqli_query($db, $query) == false) echo "Insert Failed";
+        }
+        else
+        {
+            echo "Error";
+        }
+    }
 ?>
