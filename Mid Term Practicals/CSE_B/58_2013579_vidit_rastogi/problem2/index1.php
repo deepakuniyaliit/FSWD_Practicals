6b3adca7fd27896f282b31195d0694a08dfcb3a9
unknown <viditr111@gmail.com>
2021-03-26 11:21:57 +0530

    validate data using PHP

--- Mid Term Practicals/CSE_B/58_2013579_vidit_rastogi/problem2/index1.php
+++ Mid Term Practicals/CSE_B/58_2013579_vidit_rastogi/problem2/index1.php
@@ -1,14 +1,37 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
 {
-    $username = $_POST['username'];
-    $contact=$_POST['phone'];
-    $email = $_POST['email'];
     
-    $gender = $_POST['gender'];
+    function isEmpty($text)
+    {
+        if(strlen($text)>0)
+        {
+            return $text;
+        }
+        else{
+            return "empty";
+        }
+    }
+    function isPhone($text)
+    {
+        if(strlen($text)==10)
+        {
+            return $text;
+        }
+        else{
+            return "invalid";
+        }
+    }
+    $username =isEmpty($_POST['username']); 
+    $contact=isPhone( $_POST['phone']);
+    $email = isEmpty($_POST['email']) ;
+    
+    $gender =isEmpty($_POST['gender']);
   
-    $city = $_POST['city'];
+    $city = isEmpty( $_POST['city']);
     echo "name:$username city:$city phone:$contact email:$email gender:$gender";
+
+
 }
 
 ?>
