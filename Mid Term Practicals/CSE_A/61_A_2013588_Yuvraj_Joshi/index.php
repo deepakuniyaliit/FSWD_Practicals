950202ec1cdde5a7fbdcc9ab38e9ffbd6d0fa2fa
YuvrajJoshi <yuvrajjoshi31@gmail.com>
2021-03-26 11:00:05 +0530

    retrieve form data while avoiding form submission on refreshing the page

---
+++ Mid Term Practicals/CSE_A/61_A_2013588_Yuvraj_Joshi/index.php
@@ -0,0 +1,12 @@
+<?php
+	include_once('configuration.php');
+
+	if(isset($_POST['submit'])){
+		$username = $_POST['username'];
+		$email = $_POST['email'];
+		$contact = $_POST['contact'];
+		$gender = $_POST['gender'];
+		$city = $_POST['city'];
+		header("location : index.php");
+	}
+?>
