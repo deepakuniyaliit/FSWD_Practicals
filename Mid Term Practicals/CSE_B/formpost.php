69c9f8a59fadf9a145e9a5f4f8c1a7d3944e2723
muskangupta1006 <78811268+muskangupta1006@users.noreply.github.com>
2021-03-26 10:30:34 +0530

    modify the html form to use post method

---
+++ Mid Term Practicals/CSE_B/formpost.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title> form </title>
+        
+</head>
+<body>
+  <form name="form1" method="get" action="form.php" onsubmit="return validateForm()">
+    USERNAME <input type="text" name="username" placeholder="type your username" required><br>
+    EMAIL <input type="email" name="email" placeholder="type your email" required><br>
+    CONTACT <input type="text" name="contact" placeholder="type your contact" required><br>
+    GENDER  <input type="radio" name="male" value="male">male
+    <input type="radio" name="male" value="female">female
+    <input type="radio" name="male" value="other">other
+    <br>
+    CITY<select name="city">
+      <option value="dehradun">dehradun</option>
+      <option value="delhi">delhi</option>
+      <option value="jaipur">jaipur</option>
+      <option value="mussorie">Mussorie</option>
+      <option value="nanital">Nanital</option>
+      <option value="chennai">Chennai</option>
+      <option value="banglore">Banglore</option>
+      <option value="hyderabad">Hyderabad</option>
+      <option value="lucknow">Lucknow</option>
+    </select><br>
+    <input type="submit" name="submit" value="submit">
+  </form>
+</body>
+</html>
+    
