23a5ff37e30d2cf41287a51a5a85635e28897f97
Kavya Pandey <57443445+kavya-pandey@users.noreply.github.com>
2021-03-26 10:13:48 +0530

    create a database connection using PHP

---
+++ Mid Term Practicals/CSE_A/32_2013359_Kavya_Pandey/configuration.php
@@ -0,0 +1,13 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$databasename="web";
+$conn = mysqli_connect($hostname, $username, $password, $databasename);
+if($conn) {
+    echo "Connection Successful...";
+}
+else {
+    echo "Connection failed.." + mysqli_connect_error();
+}
+?>
