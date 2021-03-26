13044909bb1113b0169cc3165f54381235cc366d
Raghavendra <raghavendrashekhawat2@gmail.com>
2021-03-26 10:31:44 +0530

    Update input_form.php
    Updated validation in html form

--- Mid Term Practicals/CSE_A/2013435_Raghavendra_Singh_Shekhawat/input_form.php
+++ Mid Term Practicals/CSE_A/2013435_Raghavendra_Singh_Shekhawat/input_form.php
@@ -5,16 +5,15 @@
     </style>
     <h3> Student Information </h3><br>
 
-
 <div class="container" style="position:relative; height:500px; width: 550px; top: 150px; left: 50px;">
-    <form action="index.php" method="POST">
+    <form action="index.php" name="myform" method="POST">
         
         <label for="username"> Enter Username: </label> 
-        <input type="text" id="username" name="username" autofocus="on" placeholder="Username"> <br>
+        <input type="text" id="username" name="username" autofocus="on" placeholder="Username" required> <br>
         <label for="email"> Enter Email: </label>
-        <input type="text" id="email" name="email" placeholder="Email"><br>
+        <input type="text" id="email" name="email" placeholder="Email" pattern=".+@gmail.com" required><br>
         <label for="contact">Contact</label> 
-        <input type="number" id="contact" name="contact" placeholder="contact"><br>
+        <input type="tel" id="contact" name="contact" placeholder="contact" pattern="[0-9]{10}"><br>
         <label for="gender">Gender</label>
         <input type="radio" name="gender" id="Male" value = "Male">
         <label for="Male">Male</label>
@@ -35,3 +34,6 @@
         <input type="submit" name="submit" value="submit">
     </form>
 </div>
+
+ 
+
