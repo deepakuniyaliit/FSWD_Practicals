23989dee00eff603f510f175e968facfc54299c2
Siddhartha03112000 <siddhartha.khandelwal0311@gmail.com>
2021-03-26 09:46:20 +0530

    created html form with get method

---
+++ Mid Term Practicals/CSE_A/57_A_20134525_Siddhartha_Khandelwal/html_form.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>HTML FORM</title>
+    </head>
+    <body>
+        <form method="GET" action="html_form.php">
+            username
+            <input type="text" name="username" placeholder="UserName" required><br>
+            email
+            <input type="email" name="email" placeholder="email" required><br>
+            contact
+            <input type="number" name="contact no." placeholder="contact no." minlength="10" maxlength="10" required ><br>
+            gender
+            Male<input type="radio" name="gender" value="male">
+            Female<input type="radio" name="gender" value="female">
+            others<input type="radio" name="gender" value="male"><br>
+            City
+            <select name="City" required>
+            <option value="">City</option>
+            <option value="Mumbai">Mumbai</option>
+            <option value="Delhi">Delhi</option>
+            <option value="Jaipur">Jaipur</option>
+            <option value="Mussoorie">Mussoorie</option>
+            <option value="Nainital">Nainital</option>
+            <option value="Chennai">Chennai</option>
+            <option value="Pune">Pune</option>
+            <option value="Banglore">Banglore</option>
+            <option value="Hyderabad">Hyderabad</option>
+            <option value="Lucknow">Lucknow</option>
+            Submit
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
