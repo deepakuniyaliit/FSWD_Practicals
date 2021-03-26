06b1461261afd896d5bb3ae16a640c09128b147a
Siddhartha03112000 <65247431+Siddhartha03112000@users.noreply.github.com>
2021-03-26 10:21:05 +0530

    created database connection using php

---
+++ Mid Term Practicals/CSE_A/57_A_20134525_Siddhartha_Khandelwal/configuration.php
@@ -0,0 +1,16 @@
+<?php
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'WebPractical';
+
+    $conn = mysqli_connect($hostname, $username, $password, $dbname);
+   
+    if($conn){
+        echo "Connection Successful";
+    }
+    else
+    {
+        echo "Connection Failed" + mysqli_connect_error();
+    }
+?>
