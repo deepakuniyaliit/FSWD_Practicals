77cca520f4755816c84030c780b63d626d04350b
unknown <viditr111@gmail.com>
2021-03-26 11:10:27 +0530

    create a table webpractical

--- Mid Term Practicals/CSE_B/58_2013579_vidit_rastogi/problem2/configuration.php
+++ Mid Term Practicals/CSE_B/58_2013579_vidit_rastogi/problem2/configuration.php
@@ -1,14 +1,31 @@
 <?php
-$hostname = "localhost";
+$servername = "localhost";
 $username = "root";
 $password = "";
-$databasename = "WebPractical";
+$dbname = "webpractical";
 
-$conn = mysqli_connect($hostname, $username, $password, $databasename);
-if($conn){
-    echo "Connection Successful...";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
-else{
-    echo "Connection failed..." + mysqli_connect_error();
+
+// sql to create table
+$sql = "CREATE TABLE users (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(30) NOT NULL,
+email VARCHAR(50) NOT NULL,
+contact INTEGER(10) NOT NULL,
+gender VARCHAR(7) NOT NULL,
+city VARCHAR(50) NOT NULL
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
 }
+
+$conn->close();
 ?>
