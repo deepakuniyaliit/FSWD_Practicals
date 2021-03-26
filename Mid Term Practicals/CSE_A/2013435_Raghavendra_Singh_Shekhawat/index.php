4419dc240fad7f44ac912e10353f4943193a141a
Raghavendra <raghavendrashekhawat2@gmail.com>
2021-03-26 11:04:52 +0530

    Update index.php
    Updated index.php to insert data in the database

--- Mid Term Practicals/CSE_A/2013435_Raghavendra_Singh_Shekhawat/index.php
+++ Mid Term Practicals/CSE_A/2013435_Raghavendra_Singh_Shekhawat/index.php
@@ -1,18 +1,35 @@
 <?php
     // Form Validation
     
+
     if(isset($_POST['submit'])){
          // Email
-        echo "DATA RECIEVED VIA POST !!";
+        
+        
         $username = $_POST['username'];
         $contact = $_POST['contact'];
         $email = $_POST['email'];
         $gender = $_POST['gender'];
         $city = $_POST['city'];
-         
-        echo "<br>" .$contact ."\t" . $username ."\t" .$gender . "\t" .$email ."\t" .$city;
+
+
+        //echo "<br>" .$contact ."\t" . $username ."\t" .$gender . "\t" .$email ."\t" .$city;
+        include('configurations.php');
+        $insert_data = "INSERT INTO `users` (`username`, `email`, `contact`, `gender`, `city`) VALUES ('$username', '$email', '$contact', '$gender', '$city' );";
+        echo "<br>" .$insert_data;
+        if(!mysqli_query($conn, $insert_data)){
+            echo "Data Not Inserted !!!";
+        }
+        else{
+              echo "Data Inserted Successfully !!!";
+        }
     }
+
+
 ?>
+
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -20,5 +37,6 @@
 <?php include('templates/header.php'); ?>
 
 <?php include('input_form.php'); ?>
+
 
 </html>
