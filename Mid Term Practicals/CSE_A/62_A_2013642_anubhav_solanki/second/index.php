ad537aa9975c9ac78c10d7b274d6d3350a380b98
anubhavs <anubhavsolanki0@gmail.com>
2021-03-26 11:14:42 +0530

    retrieve data from users table using PHP script

--- Mid Term Practicals/CSE_A/62_A_2013642_anubhav_solanki/second/index.php
+++ Mid Term Practicals/CSE_A/62_A_2013642_anubhav_solanki/second/index.php
@@ -20,3 +20,36 @@ validation($username,$email,$contact,$gender,$city);
     $sql = "INSERT INTO `users` (`id`, `username`, `email`, `gender`, `city`, `contact`) VALUES (NULL, '$username', '$email', '$gender', '$city','$contact')";
     mysqli_query($conn,$sql);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+       $sql = "SELECT * FROM `user`";
+       $data = $conn->query($sql);
+       if($data->num_rows > 0){
+           $i=0;
+           while($row = $data->fetch_assoc()){
+               $i = $i + 1;
+    ?>
+            <div>
+            <?php echo $i?>. Details of user with ID = <?php echo $row["id"]?> 
+            <ul>
+                <li>Username  - <?php echo $row["username"]?> </li>
+                <li>Email - <?php echo $row["email"]?></li>
+                <li>Contact - <?php echo $row["contact"]?></li>
+                <li>Gender - <?php echo $row["gender"]?></li>
+                <li>City - <?php echo $row["city"]?></li>
+            </ul>
+            </div>
+    <?php
+    }
+    ?>
+</body>
+</html>
