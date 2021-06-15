541bdf8d57336cce13968206d567021faeced245
Rishav <rishavkumar79@gmail.com>
2021-06-15 10:07:49 +0530

    Second question

---
+++ End Term Practicals/CSEC/42_2013450_RISHAV_GUPTA/Ques 2/ques2.php
@@ -0,0 +1,43 @@
+<?php
+
+  $db = mysql_connect("localhost", "root", "");
+  $er = mysql_select_db("BookStore");
+  $query = "insert into names values('$Book','$Book Name','$Author','ISBN','$Publisher','$Publication Year','$Pages','$Book Type')";
+  $result = mysql_query($query);
+  print "<p> Person's Information Inserted </p>";
+  $result = mysql_query("SELECT * FROM names");
+?>
+
+<table border="2">
+   <tr>
+      <th>Book ID</th>
+      <th>Book Name</th>
+      <th>Author</th>
+      <th>ISBN</th>
+      <th>Publisher</th> 
+      <th>Publication Year</th>
+      <th>Pages</th>
+      <th>Book Type</th>
+    </tr>
+    <? 
+    while ($array = mysql_fetch_row($result));
+    {
+        print "<tr> <td>";
+        echo $array[0]; 
+        print "</td> <td>";
+        echo $array[1]; 
+        print "</td> <td>";
+        echo $array[2]; 
+        print "</td> <td>";
+        echo $array[3]; 
+        print "</td> <td>";
+        echo $array[4]; 
+        print "</td> <td>";
+        echo $array[5]; 
+        print "</td> <td>";
+        echo $array[6]; 
+        print "</td> <td>";
+        echo $array[7]; 
+        print "</td> </tr>";
+    }
+?>
