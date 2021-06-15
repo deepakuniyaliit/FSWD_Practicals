fa151355f024bc4eaa5da89ad240eaccd399b475
Pragya-singh24 <pragyasingh0268@gmail.com>
2021-06-15 10:05:33 +0530

    Myfiles

---
+++ End Term Practicals/CSECC/11_2013420_Pragya_Singh/Q2/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$hostname="localhost"; 
+$username = "root";
+$password = "";
+$databasename = "web1";
+
+$conn = mysqli_connect($hostname, $username, $password, $databasename);
+
+if($conn){ 
+    echo "Connection Successful";
+}
+else{
+echo "Connection failed" + mysqli_connect_error();
+}
+
+?>
