95cd1ed71767e3107be77a35f0dfbd10e32186f1
Ashish <ashishbisht15january12345@gmail.com>
2021-06-15 09:57:39 +0530

    change

--- End Term Practicals/CSECC/19_2013613_Ashish_Bisht/Q1/server.php
+++ End Term Practicals/CSECC/19_2013613_Ashish_Bisht/Q1/server.php
@@ -6,14 +6,14 @@ if(!$conn)
     die("Connection   Failed..!!");
 }
 
-$s = "CREATE TABLE IF NOT EXISTS RECORD {BookName varchar(20),AuthName varchar(20),isbnNo int,PubName varchar(20),PubYear int,BookType varchar(20)}";
+$s = "CREATE TABLE IF NOT EXISTS record {BookName varchar(20),AuthName varchar(20),isbnNo int,PubName varchar(20),PubYear int,BookType varchar(20)}";
 if(!mysqli_query($conn,$s))
 {
   die("SQL Error");
 }
 else
 {
-    $s2="INSERT INTO RECORD (BookName,AuthName,isbnNo,PubName,PubYear,BookType) values($_POST['book_name'],$_POST['auth_name'],$_POST['isbn_no'],$_POST['pub_name'],$_POST['year'],$_POST['noOfPages'],$_POST['type'])";
+    $s2="INSERT INTO record (BookName,AuthName,isbnNo,PubName,PubYear,BookType) values('$_POST["book_name"]','$_POST["auth_name"]','$_POST["isbn_no"]','$_POST["pub_name"]','$_POST["year"]','$_POST["noOfPages"]','$_POST["type"]')";
     if(!mysqli_query($conn,$s2))
     die("SQL Error");
     else
