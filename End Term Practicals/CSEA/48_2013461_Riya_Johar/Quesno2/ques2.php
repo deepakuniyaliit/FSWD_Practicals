307d1e471a623479b4e9773591d4745969f057ce
Riya122 <70003575+Riya122@users.noreply.github.com>
2021-06-15 09:54:19 +0530

    Update ques2.php

--- End Term Practicals/CSEA/48_2013461_Riya_Johar/Quesno2/ques2.php
+++ End Term Practicals/CSEA/48_2013461_Riya_Johar/Quesno2/ques2.php
@@ -5,7 +5,7 @@ include('config.php');
 <!-- Code to fetch data -->
 <?php
 
-    $sql="SELECT * FROM users";
+    $sql="SELECT * FROM BookDetails";
     $result=mysqli_query($conn, $sql);
 
     if($result!==false && $result->num_rows > 0){
@@ -37,9 +37,6 @@ include('config.php');
                 <td><?php echo $row[5]?></td>
                 <td><?php echo $row[6]?></td>
                 <td><?php echo $row[7]?></td>
-                <td><a href="edit.php?id=<?php echo $row[0]?>">
-                    <input type="button" value="Edit"></a></td>
-                <td><input type="button" value="delete"></td>
 
             </tr>
             <?php } ?>
