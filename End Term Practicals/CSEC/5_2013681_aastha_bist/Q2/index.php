1787cdf7d2d670ee9f837ab59f8f1f7bf26e9b0a
Aastha Bist <abist119@gmail.com>
2021-06-15 10:06:10 +0530

    Practical ques

---
+++ End Term Practicals/CSEC/5_2013681_aastha_bist/Q2/index.php
@@ -0,0 +1,40 @@
+<?php
+$user ='root';
+$password ='';
+$db='bookstore';
+$con=mysqli_connect("localhost",$user,$password,$db);
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$res = mysqli_query($con,"SELECT * FROM red");
+
+echo "<table border='2'>
+<tr>
+<th>Book Name</th>
+<th> Author Name</th>
+<th> ISBN number</th>
+<th> No of pages</th>
+<th> Publisher name</th>
+<th> Publication year</th>
+<th>book type</th>
+</tr>";
+
+while($row = mysqli_fetch_array($res))
+{
+echo "<tr>";
+echo "<td>" . $row['bookname'] . "</td>";
+echo "<td>" . $row['authorname'] . "</td>";
+echo "<td>" . $row['isbnno'] . "</td>";
+echo "<td>" . $row['noofpages'] . "</td>";
+echo "<td>" . $row['publishername'] . "</td>";
+echo "<td>" . $row['publicationyear'] . "</td>";
+echo "<td>" . $row['booktype'] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+?>
