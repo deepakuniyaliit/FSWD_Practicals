221f643a377e24b9a3d25299185951e6417654b9
ankitpanwar <ap11821141@gmail.com>
2021-06-15 09:53:31 +0530

    add question 2

--- End Term Practicals/IT/35_2013749_Ankit_Panwar/Q1/form.php
+++ End Term Practicals/IT/35_2013749_Ankit_Panwar/Q1/form.php
@@ -8,7 +8,7 @@ if(isset($_POST['submit']))
     $publisher = $_POST['publisherName'];
     $year= $_POST['publicationYear'];
     $booktype= $_POST['bookType'];
-    $pages= $_POST['noOfPages'];
+    $pages= $_POST['Pages'];
 
     $sql = "INSERT INTO table1 (bookName, authorName, isbnNumber, publisherName, publicationYear, noOfPages, bookType) VALUES ('$bookname', '$authorname', '$isbn', '$publisher', '$year', '$pages', '$booktype')";
     mysqli_query($conn, $sql);
