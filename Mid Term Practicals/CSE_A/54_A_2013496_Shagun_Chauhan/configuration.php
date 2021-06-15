b28bc078d4b7ebe7000f81ba658e5a84967d0935
Ritika Tripathi <retecatripathi@gmail.com>
2021-06-15 23:49:23 +0530

    endterm

--- Mid Term Practicals/CSE_A/54_A_2013496_Shagun_Chauhan/configuration.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "WebPractical";
-    
-    $conn = mysqli_connect($hostname, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
-?>
