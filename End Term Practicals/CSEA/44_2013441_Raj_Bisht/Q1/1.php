da8579c648f2f3d8905e368d50b9da5287d88c4d
Raj-bisht <61263677+Raj-bisht@users.noreply.github.com>
2021-06-15 10:15:23 +0530

    Ques 1

--- End Term Practicals/CSEA/44_2013441_Raj_Bisht/Q1/1.php
+++ End Term Practicals/CSEA/44_2013441_Raj_Bisht/Q1/1.php
@@ -28,7 +28,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Book</title>
 
 </head>
 <body>
