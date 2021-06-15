e4d4d6e3e9a42959bb5efe985839b8eaa6c4c0b9
Priyanka Koranga <47287551+priyankakoranga@users.noreply.github.com>
2021-06-15 10:01:25 +0530

    Add files via upload

---
+++ End Term Practicals/CSEDS/11_2013430_priyanka_koranga/ques2/books.php
@@ -0,0 +1,30 @@
+<?
+  $db = mysql_connect("localhost", "root", "");
+  $er = mysql_select_db("ram");
+  $query = "insert into names values('$name','$add1','$add2','$mail')";
+  $result = mysql_query($query);
+  print "<p> Person's Information Inserted </p>";
+  $result = mysql_query("SELECT * FROM names");
+?>
+
+<table border="2">
+   <tr>
+      <th>Name</th>
+      <th>Address Line 1</th>
+      <th>Address Line 2 </th>
+      <th>E-mail Id </th>
+    </tr>
+    <? 
+    while ($array = mysql_fetch_row($result));
+    {
+        print "<tr> <td>";
+        echo $array[0]; 
+        print "</td> <td>";
+        echo $array[1]; 
+        print "</td> <td>";
+        echo $array[2]; 
+        print "</td> <td>";
+        echo $array[3]; 
+        print "</td> </tr>";
+    }
+?>
